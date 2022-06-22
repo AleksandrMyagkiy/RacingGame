eee1a3d938b2d3f7064568b741028e78ee5a845f
Aleksandr <dentranscom@gmail.com>
2022-06-22 21:28:37 +0300

    add: speed

--- src/scripts/classes/Player.js
+++ src/scripts/classes/Player.js
@@ -1,7 +1,7 @@
 const DIRECTIONS = Object.freeze({BACKWARD: -1, NONE: 0, FORWARD: 1});
 const TURNS = Object.freeze({LEFT: -1, NONE: 0, RIGHT: 1});
 const SPEED = 10;
-
+const ACCELERATION = 0.5;
 export default class Player {
     constructor(scene, map) {
         this.scene = scene;
@@ -11,6 +11,7 @@ export default class Player {
         // помещаем спрайт машины в нужную позицию на сцене
         this.car = this.scene.matter.add.sprite(position.x, position.y, 'objects', 'car_blue_1');
         this.car.setFixedRotation(true); // отменяем самовольное вращение при столкновении
+        this._velocity = 0; // реальная скорость машины в данный момент времени
     }
     get direction() {
         let direction = DIRECTIONS.NONE;
@@ -24,7 +25,14 @@ export default class Player {
         return direction;
     }
     get velocity() {
-        return this.direction * SPEED;
+        const speed = Math.abs(this._velocity); // получаем в speed текущую скорость без учета направления авто
+
+        if (this.direction && speed < SPEED) { // если нажата кнопка движения вперед или назад и скорость не достигла максимальной
+            this._velocity += ACCELERATION * Math.sign(this.direction); // тогда прибавляем к значение скорости 0,5 до тех пор пока работает условие
+        } else if (!this.direction && speed > 0) { // если же кнопка движение не нажата и скорость больше 0
+            this._velocity -= ACCELERATION * Math.sign(this._velocity); // тогда уменьшаем скорость на 0,5 до полного остановления
+        }
+        return this._velocity;
     }
     get turn() {
         let turn = TURNS.NONE;
@@ -38,7 +46,7 @@ export default class Player {
         return turn;
     }
     get angle() {
-        return this.car.angle + this.turn * SPEED / 2;
+        return this.car.angle + this.turn * SPEED / 3;
     }
     getVelocityFromAngle() {
         const vec2 = new Phaser.Math.Vector2();
